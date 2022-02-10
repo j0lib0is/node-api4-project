9d1070895d33b6d1a0b678ea02e820fcb2518514
jacobjolibois <jacobjolibois@hey.com>
2022-02-10 16:25:44 -0600

    reoganied my index.js file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const server = express();
 
-require('dotenv').config();
-const PORT = process.env.PORT || 8080;
-
 server.get('/api/users', (req, res) => {
 	res.json([
 		{name: 'John', id: 1},
@@ -11,6 +8,9 @@ server.get('/api/users', (req, res) => {
 		{name: 'Jill', id: 3}
 	]);
 });
+
+require('dotenv').config();
+const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}.`);
