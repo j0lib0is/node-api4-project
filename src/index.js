03f00f3715392458ad2d17fc3f421b099271ad90
jacobjolibois <jacobjolibois@hey.com>
2022-02-10 16:17:17 -0600

    changed returned object to a returned array

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ require('dotenv').config();
 const PORT = process.env.PORT || 8080;
 
 server.get('/api/users', (req, res) => {
-	res.json({name: 'Jacob', id: 1});
+	res.json([
+		{name: 'John', id: 1},
+		{name: 'Jack', id: 2},
+		{name: 'Jill', id: 3}
+	]);
 });
 
 server.listen(PORT, () => {
